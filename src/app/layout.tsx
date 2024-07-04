17c06aa8bebb8f4ad2fad4f160143f1638afa998
Yeison Molina <yeison.molina91@gmail.com>
2024-07-04 18:38:43 -0500

    Improve layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import type { ReactNode } from 'react'
 import type { Metadata } from 'next'
-import dynamic from 'next/dynamic'
-import { Roboto } from 'next/font/google'
 import '@/app/styles/globals.scss'
 
+import { AppLayout } from '@/app/layouts/app'
 import { APP_DESCRIPTION, APP_TITLE } from '@/domain/constants/app'
-
-const AppLayout = dynamic(() => import('@/app/layouts/app/AppLayout'), { ssr: false })
-
-const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-roboto',
-})
 
 export const metadata: Metadata = {
   title: APP_TITLE,
@@ -26,15 +17,8 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
-    <html lang="en">
-      <body
-        suppressHydrationWarning
-        className={ `${roboto.className}` }
-      >
-        <AppLayout>
-          {children}
-        </AppLayout>
-      </body>
-    </html>
+    <AppLayout>
+      { children }
+    </AppLayout>
   )
 }
--- src/ui/layouts/app/AppLayout.tsx
+++ src/ui/layouts/app/AppLayout.tsx
@@ -1,24 +1,41 @@
 'use client'
 
 import React from 'react'
+import dynamic from 'next/dynamic'
 import { ThemeProvider } from '@material-tailwind/react'
 import { SnackbarProvider } from 'notistack'
+import { Roboto } from 'next/font/google'
 
-import { UserProvider } from '@/domain/contexts/user'
 import type { AppLayoutProps } from './types'
 import styles from './styles.module.scss'
 
+const UserProvider = dynamic(() => import('@/domain/contexts/user/UserProvider'), { ssr: false })
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+})
+
 export function AppLayout({ children }: Readonly<AppLayoutProps>) {
   return (
-    <ThemeProvider>
-      <SnackbarProvider maxSnack={ 3 }>
-        <UserProvider>
-          <main className={ styles.main }>
-            { children }
-          </main>
-        </UserProvider>
-      </SnackbarProvider>
-    </ThemeProvider>
+    <html lang="en">
+      <body
+        suppressHydrationWarning
+        className={ `${roboto.className}` }
+        data-testid="app-layout-body"
+      >
+        <ThemeProvider>
+          <SnackbarProvider maxSnack={ 3 }>
+            <UserProvider>
+              <main className={ styles.main }>
+                { children }
+              </main>
+            </UserProvider>
+          </SnackbarProvider>
+        </ThemeProvider>
+      </body>
+    </html>
   )
 }
 
