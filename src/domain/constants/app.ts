54aa8183a4ac7bdd245655eb0cd2d3b46a58f30a
Yeison Molina <yeison.molina91@gmail.com>
2024-07-04 18:37:10 -0500

    Add regex email

--- src/domain/constants/app.ts
+++ src/domain/constants/app.ts
@@ -2,3 +2,4 @@ export const APP_TITLE = 'User Profile'
 export const APP_DESCRIPTION = 'Profile Page for User with Next.js 14'
 export const BANNER_WIDTH = 1920
 export const BANNER_HEIGHT = 500
+export const REGEX_EMAIL = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
